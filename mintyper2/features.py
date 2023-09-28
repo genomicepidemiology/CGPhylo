055ae7e3716978339c3fe616d941bcada89ae33e
MBHallgren <s153002@student.dtu.dk>
2023-09-28 12:44:51 +0200

    push3

--- mintyper2/features.py
+++ mintyper2/features.py
@@ -36,24 +36,25 @@ def produce_kmers(args):
     """Produces kmer files for the input file."""
     for item in args.illumina:
         name = item.split('/')[-1].split('.')[0]
-        os.system('kmc -k1 -cs1000000000000 -b {} output/1mers . > output/{}_1mers_stats.txt'.format(item, name))
-        os.system('kmc_dump output/1mers output/1mers.txt')
-        os.system('kmc -k5 -cs1000000000 {} output/5mers . > output/{}_5mers_stats.txt'.format(item, name))
-        os.system('kmc_dump output/5mers output/5mers.txt')
-        os.system('kmc -k13 -cs1000000000 {} output/13mers . > output/{}_13mers_stats.txt'.format(item, name))
-        os.system('kmc_dump output/13mers output/13mers.txt')
-        os.system('kmc -k21 -cs1000000000 {} output/21mers . > output/{}_21mers_stats.txt'.format(item, name))
-        os.system('kmc_dump output/21mers output/21mers.txt')
+        os.system('kmc -k1 -cs1000000000000 -b {} output/{}_1mers . > output/{}_1mers_stats.txt'.format(item, name, name))
+        os.system('kmc_dump output/{}_1mers output/{}_1mers.txt'.format(name, name))
+        os.system('kmc -k5 -cs1000000000 {} output/{}_5mers . > output/{}_5mers_stats.txt'.format(item, name, name))
+        os.system('kmc_dump output/{}_5mers output/{}_5mers.txt'.format(name, name)
+        os.system('kmc -k13 -cs1000000000 {} output/{}_13mers . > output/{}_13mers_stats.txt'.format(item, name, name))
+        os.system('kmc_dump output/{}_13mers output/{}_13mers.txt'.format(name, name))
+        os.system('kmc -k21 -cs1000000000 {} output/{}_21mers . > output/{}_21mers_stats.txt'.format(item, name, name))
+        os.system('kmc_dump output/{}_21mers output/{}_21mers.txt'.format(name, name))
+
     for item in args.nanopore:
         name = item.split('/')[-1].split('.')[0]
-        os.system('kmc -k1 -cs1000000000000 -b {} output/1mers . > output/{}_1mers_stats.txt'.format(item, name))
-        os.system('kmc_dump output/1mers output/1mers.txt')
-        os.system('kmc -k5 -cs1000000000 {} output/5mers . > output/{}_5mers_stats.txt'.format(item, name))
-        os.system('kmc_dump output/5mers output/5mers.txt')
-        os.system('kmc -k13 -cs1000000000 {} output/13mers . > output/{}_13mers_stats.txt'.format(item, name))
-        os.system('kmc_dump output/13mers output/13mers.txt')
-        os.system('kmc -k21 -cs1000000000 {} output/21mers . > output/{}_21mers_stats.txt'.format(item, name))
-        os.system('kmc_dump output/21mers output/21mers.txt')
+        os.system('kmc -k1 -cs1000000000000 -b {} output/{}_1mers . > output/{}_1mers_stats.txt'.format(item, name, name))
+        os.system('kmc_dump output/{}_1mers output/{}_1mers.txt'.format(name, name))
+        os.system('kmc -k5 -cs1000000000 {} output/{}_5mers . > output/{}_5mers_stats.txt'.format(item, name, name))
+        os.system('kmc_dump output/{}_5mers output/{}_5mers.txt'.format(name, name)
+        os.system('kmc -k13 -cs1000000000 {} output/{}_13mers . > output/{}_13mers_stats.txt'.format(item, name, name))
+        os.system('kmc_dump output/{}_13mers output/{}_13mers.txt'.format(name, name))
+        os.system('kmc -k21 -cs1000000000 {} output/{}_21mers . > output/{}_21mers_stats.txt'.format(item, name, name))
+        os.system('kmc_dump output/{}_21mers output/{}_21mers.txt'.format(name, name))
 def read_file_to_set(filename):
     """Reads kmer strings from a file and returns a set of those kmers."""
     kmers = set()
