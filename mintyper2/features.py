8bc0389128bacc77fcbee248d5643754ea277af4
MBHallgren <s153002@student.dtu.dk>
2023-09-28 10:01:28 +0200

    kmc5

--- mintyper2/features.py
+++ mintyper2/features.py
@@ -4,4 +4,9 @@ import sys
 def produce_features(args):
     os.mkdir('output')
     os.system('kmc -k5 {} output/5mers .'.format(args.input[0]))
+    os.system('kmc_dump output/5mers output/5mers.txt')
+    os.system('kmc -k13 {} output/13mers .'.format(args.input[0]))
+    os.system('kmc_dump output/13mers output/13mers.txt')
+    os.system('kmc -k21 {} output/21mers .'.format(args.input[0]))
+    os.system('kmc_dump output/21mers output/21mers.txt')
     return 'test'
