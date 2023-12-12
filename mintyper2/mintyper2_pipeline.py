8245712e96ae87cb9b5d0f7060cdc91cfb822cdb
MBHallgren <s153002@student.dtu.dk>
2023-12-12 21:38:43 +0100

    genes22

--- mintyper2/mintyper2_pipeline.py
+++ mintyper2/mintyper2_pipeline.py
@@ -43,13 +43,13 @@ def calculate_pairwise_distances(sequences_dict):
 
                 diff = sum(1 for a, b in zip(seq1, seq2) if a != b)
 
-                if diff > 30:
-                    print ('-----')
-                    print (gene)
-                    print (seq1)
-                    print (seq2)
-                    print (diff)
-                    print ('-----')
+                #if diff > 30:
+                #    print ('-----')
+                #    print (gene)
+                #    print (seq1)
+                #    print (seq2)
+                #    print (diff)
+                #    print ('-----')
                 # Count differences
                 count += diff
 
