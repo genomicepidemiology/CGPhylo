0d1932eff72ad4775f9c3a796d136d299fba837f
MBHallgren <s153002@student.dtu.dk>
2023-12-13 14:02:13 +0100

    n5

--- mintyper2/mintyper2_pipeline.py
+++ mintyper2/mintyper2_pipeline.py
@@ -5,21 +5,19 @@ def mintyper2_pipeline(args):
     """Main function"""
     os.system('mkdir {}'.format(args.output))
     #Find species
-    """
     if args.nanopore != []:
         for file in args.nanopore:
             if len(file.split(' ')) == 1:
                 name = file.split('/')[-1].split('.')[0]
             else:
                 name = file.split(' ')[0].split('/')[-1].split('.')[0]
-            cmd = 'kma -i {} -o {}/{} -t_db /home/people/malhal/mintyper2/consensus_genes_db -ID 50 -md 5 -mct 0.5 -t 8 -mem_mode -dense -ref_fsa -ont'.format(file, args.output, name)
+            cmd = 'kma -i {} -o {}/{} -t_db /home/people/malhal/mintyper2/consensus_genes_db -ID 90 -md 5 -mct 0.5 -t 8 -mem_mode -dense -ref_fsa -ont'.format(file, args.output, name)
             os.system(cmd)
     if args.illumina != []:
         for i in range(0, len(args.illumina), 2):
             name = args.illumina[i].split('/')[-1].split('.')[0]
-            cmd = 'kma -i {} {} -o {}/{} -t_db /home/people/malhal/mintyper2/consensus_genes_db -ID 50 -mct 0.5 -md 5 -mem_mode -dense -ref_fsa -t 8'.format(args.illumina[i], args.illumina[i+1], args.output, name)
+            cmd = 'kma -i {} {} -o {}/{} -t_db /home/people/malhal/mintyper2/consensus_genes_db -ID 90 -mct 0.5 -md 5 -mem_mode -dense -ref_fsa -t 8'.format(args.illumina[i], args.illumina[i+1], args.output, name)
             os.system(cmd)
-    """
     gene_list = find_common_genes(args.output)
     print (gene_list)
     print (len(gene_list), 'genes shared between all samples')
