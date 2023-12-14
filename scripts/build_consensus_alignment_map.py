4ef54d8997ebbbbfb1ffc33597b1ba6ac6e1304f
MBHallgren <s153002@student.dtu.dk>
2023-12-14 09:48:45 +0100

    n3

--- scripts/build_consensus_alignment_map.py
+++ scripts/build_consensus_alignment_map.py
@@ -11,20 +11,24 @@ def align_sequences(fasta_file):
 
     # Step 2: Identify the longest sequence for each gene
     longest_seqs = {}
-    for header, seq in sequences.items():
+    for header, seq_record in sequences.items():
         gene_name = header.split('_')[0]
-        if gene_name not in longest_seqs or len(seq) > len(longest_seqs[gene_name]):
-            longest_seqs[gene_name] = seq
+        seq = str(seq_record.seq)  # Convert Seq object to string
+        if gene_name not in longest_seqs or len(seq) > len(longest_seqs[gene_name][1]):
+            longest_seqs[gene_name] = (header, seq)
 
     # Step 3: Align each sequence to the longest one of the same gene
     aligner = PairwiseAligner()
     aligner.mode = 'global'
     gene_alignments = {}
 
-    for header, seq in sequences.items():
+    for header, seq_record in sequences.items():
         gene_name = header.split('_')[0]
-        if header != gene_name + "_len_" + str(len(longest_seqs[gene_name])):
-            alignment = aligner.align(longest_seqs[gene_name], seq)
+        seq = str(seq_record.seq)  # Convert Seq object to string
+        longest_seq_header, longest_seq = longest_seqs[gene_name]
+
+        if header != longest_seq_header:
+            alignment = aligner.align(longest_seq, seq)
             # Step 4: Generate CIGAR string
             cigar = alignment[0].cigar
             # Step 5: Store in a nested dictionary
