d0d98b36e449e5b19c8a428b6c58e303ea80d694
MBHallgren <s153002@student.dtu.dk>
2023-12-14 09:50:51 +0100

    n5

--- scripts/build_consensus_alignment_map.py
+++ scripts/build_consensus_alignment_map.py
@@ -1,43 +1,58 @@
-import os
-import sys
 from Bio import SeqIO, Align
 from Bio.Align import PairwiseAligner
-import re
 
 
 def align_sequences(fasta_file):
-    # Step 1: Parse the FASTA file
     sequences = SeqIO.to_dict(SeqIO.parse(fasta_file, "fasta"))
-
-    # Step 2: Identify the longest sequence for each gene
     longest_seqs = {}
+
+    # Find the longest sequence for each gene
     for header, seq_record in sequences.items():
         gene_name = header.split('_')[0]
-        seq = str(seq_record.seq)  # Convert Seq object to string
+        seq = str(seq_record.seq)
         if gene_name not in longest_seqs or len(seq) > len(longest_seqs[gene_name][1]):
             longest_seqs[gene_name] = (header, seq)
 
-    # Step 3: Align each sequence to the longest one of the same gene
     aligner = PairwiseAligner()
     aligner.mode = 'global'
     gene_alignments = {}
 
+    # Align each sequence to the longest one of the same gene
     for header, seq_record in sequences.items():
         gene_name = header.split('_')[0]
-        seq = str(seq_record.seq)  # Convert Seq object to string
+        seq = str(seq_record.seq)
         longest_seq_header, longest_seq = longest_seqs[gene_name]
 
         if header != longest_seq_header:
-            alignment = aligner.align(longest_seq, seq)
-            print (alignment)
-            # Step 4: Generate CIGAR string
-            cigar = alignment[0].cigar
-            # Step 5: Store in a nested dictionary
+            alignments = aligner.align(longest_seq, seq)
+            best_alignment = alignments[0]
+            cigar = create_cigar_string(best_alignment)
+
             if gene_name not in gene_alignments:
                 gene_alignments[gene_name] = {}
             gene_alignments[gene_name][header] = cigar
 
     return gene_alignments
+
+
+def create_cigar_string(alignment):
+    cigar = []
+    match_count = 0
+    for (start, end) in zip(*alignment.aligned):
+        if match_count:
+            cigar.append(f"{match_count}M")
+            match_count = 0
+        gap_start, gap_end = start if alignment.target_start <= alignment.query_start else end
+        gap_length = gap_end - gap_start
+        if gap_length:
+            cigar.append(f"{gap_length}{'I' if alignment.target_start <= alignment.query_start else 'D'}")
+    if match_count:
+        cigar.append(f"{match_count}M")
+    return "".join(cigar)
+
+
+# Usage
+# gene_alignments = align_sequences("path_to_your_fasta_file.fasta")
 
 # Usage
 gene_alignments = align_sequences("consensus_genes.fasta")
