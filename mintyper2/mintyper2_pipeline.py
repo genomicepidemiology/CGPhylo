e8b696a0e2bd7d8b85cff0f3fea8b04039151a93
MBHallgren <s153002@student.dtu.dk>
2023-12-19 12:54:02 +0100

    realign18

--- mintyper2/mintyper2_pipeline.py
+++ mintyper2/mintyper2_pipeline.py
@@ -5,6 +5,7 @@ def mintyper2_pipeline(args):
     """Main function"""
     os.system('mkdir {}'.format(args.output))
     #Find species
+    """
 
     if args.nanopore != []:
         for file in args.nanopore:
@@ -19,7 +20,7 @@ def mintyper2_pipeline(args):
             name = args.illumina[i].split('/')[-1].split('.')[0]
             cmd = 'kma -i {} {} -o {}/{} -t_db /home/people/malhal/mintyper2/consensus_genes_db_2 -ID 90 -mct 0.5 -md 5 -mem_mode -dense -ref_fsa -t 8'.format(args.illumina[i], args.illumina[i+1], args.output, name)
             os.system(cmd)
-
+    """
     gene_list, non_shared_genes = find_common_genes(args.output)
 
     #Distance non_shared_genes
