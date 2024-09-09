f2dddd56507c7350b8930cfe3384f330f533a66e
MBHallgren <s153002@student.dtu.dk>
2024-09-09 12:18:46 +0200

    0.1.0

---
+++ make_conda.py
@@ -0,0 +1,50 @@
+import yaml
+import os
+import sys
+
+sys.path = [os.path.join(os.path.dirname(os.path.realpath(__file__)), '')] + sys.path
+
+import src.version as version
+
+data = {
+    "package": {
+        "name": "cgphylo",
+        "version": version.__version__
+    },
+    "source": {
+        "url": "https://github.com/genomicepidemiology/CGPhylo/archive/refs/tags/{}.tar.gz".format(version.__version__),
+    },
+    "build": {
+        "number": 0,
+        "noarch": "python",
+        "script": "{{ PYTHON }} -m pip install . --no-deps --ignore-installed -vvv",
+        "script_env": ["PYTHONNOUSERSITE=1"]
+    },
+    "requirements": {
+        "host": [
+            "python >=3.6",
+            "pip"
+        ],
+        "run": [
+            "kma >=1.4.9"
+        ]
+    },
+    "test": {
+        "imports": ["cgphylo"]
+    },
+    "about": {
+        "home": "https://github.com/genomicepidemiology/CGPhylo",
+        "summary": "CGPhylo - core genome phylogenetics",
+        "license": "Apache-2.0"
+    },
+    "extra": {
+        "recipe-maintainers": ["malhal"]
+    }
+}
+
+# Convert the data to YAML and print it
+os.system('mkdir -p conda')
+yaml_str = yaml.dump(data, sort_keys=False)
+
+with open('conda/meta.yaml', 'w') as f:
+    f.write(yaml_str)
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+from src import version
+__version__ = version.__version__
+###
+
+
+setup(
+    name='cgphylo',
+    version=__version__,
+    packages=find_packages(),
+    include_package_data=True,  # Ensure this is set to True
+    package_data={
+        '': ['*.json'],  # Include all json files from any package
+        'nanomgt': ['*.json'],  # Specifically include json files from the nanomgt package if needed
+    },
+    url='https://github.com/genomicepidemiology/cgphylo',
+    license='',
+    install_requires=[],
+    author='Malte B. Hallgren',
+    scripts=['bin/cgphylo'],
+    author_email='malhal@food.dtu.dk',
+    description='cgPhylo - core genome phylogenetics'
+)
